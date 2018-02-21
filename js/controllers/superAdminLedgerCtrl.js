307045535b4765a2783b67a24c25f1802d2ce17f
tsmrafee <tsmrafee@gmail.com>
2018-02-22 03:50:00 +0600

    hthgfvng

--- js/controllers/superAdminLedgerCtrl.js
+++ js/controllers/superAdminLedgerCtrl.js
@@ -31,150 +31,165 @@ myApp.controller('superAdminLedgerCtrl', function ($scope, baseSvc, $uibModal, $
         var td = "" + to.getFullYear() + "-" + (to.getMonth() + 1) + "-" + to.getDate();
         baseSvc.get("super/ledgers?from=" + fd + "&to=" + td + "&id=" + id )
             .then(function (response) {
-                $scope.ledgers = [];
-                $scope.ledgers = response;
+                $scope.ledger = {};
+                $scope.ledger = response[0];
 
-                $scope.ledgers.forEach(function (node) {
-                    node.transactions = [];
-                    if (node.opening_balance_type=="Dr") {
-                        var transaction = {};
-                        transaction.date1 = '';
-                        transaction.creditAccount = 'By Balance C/D';
-                        transaction.creditValue = node.balance;
-                        transaction.date2 = '';
-                        transaction.debitAccount = '';
-                        transaction.debitValue = '';
-                        node.transactions.push(transaction);
-                    }
-                    else if (node.cr_balance > node.dr_balance) {
-                        var transaction = {};
-                        transaction.date1 = '';
-                        transaction.creditAccount = '';
-                        transaction.creditValue = '';
-                        transaction.date2 = '';
-                        transaction.debitAccount = 'To Balance C/D';
-                        transaction.debitValue = node.balance;
-                        node.transactions.push(transaction);
-                    }
-                    node.dr = Object.keys(node.dr).map(function (k) { return node.dr[k] });
-                    node.cr = Object.keys(node.cr).map(function (k) { return node.cr[k] });
-                    if (node.cr.length > node.dr.length) {
-                        for (var i = 0; i < node.cr.length; i++) {
-                            var transaction = {};
-                            transaction.date1 = new Date(node.cr[i].date.date);
-                            transaction.debitAccount = "To " + node.cr[i].account;
-                            transaction.debitValue = node.cr[i].value;
-                            if (node.dr.length > i) {
-                                transaction.date2 = new Date(node.dr[i].date.date);
-                                transaction.creditAccount = "By " + node.dr[i].account;
-                                transaction.creditValue = node.dr[i].value;
-                            }
-                            else if (node.dr.length == i && node.dr_balance > node.cr_balance) {
-                                transaction.date2 = '';
-                                transaction.creditAccount = 'By Balance C/D';
-                                transaction.creditValue = node.balance;
-                            }
-                            else {
-                                transaction.date2 = '';
-                                transaction.creditAccount = '';
-                                transaction.creditValue = '';
-                            }
-                            node.transactions.push(transaction);
-                        }
-                        if (node.cr_balance > node.dr_balance) {
-                            var transaction = {};
-                            transaction.date1 = '';
-                            transaction.creditAccount = '';
-                            transaction.creditValue = '';
-                            transaction.date2 = '';
-                            transaction.debitAccount = 'To Balance C/D';
-                            transaction.debitValue = node.balance;
-                            node.transactions.push(transaction);
-                        }
-                    }
-                    else if (node.cr.length < node.dr.length) {
-                        for (var i = 0; i < node.dr.length; i++) {
-                            var transaction = {};
-                            transaction.date2 = new Date(node.dr[i].date.date);
-                            transaction.creditAccount = "By " + node.dr[i].account;
-                            transaction.creditValue = node.dr[i].value;
-                            if (node.cr.length > i) {
-                                transaction.date1 = new Date(node.cr[i].date.date);
-                                transaction.debitAccount = "To " + node.cr[i].account;
-                                transaction.debitValue = node.cr[i].value;
-                            }
-                            else if (node.cr.length == i && node.cr_balance > node.dr_balance) {
-                                transaction.date1 = '';
-                                transaction.debitAccount = 'To Balance C/D';
-                                transaction.debitValue = node.balance;
-                            }
-                            else {
-                                transaction.date1 = '';
-                                transaction.debitAccount = '';
-                                transaction.debitValue = '';
-                            }
-                            node.transactions.push(transaction);
-                        }
-                        if (node.cr_balance < node.dr_balance) {
-                            var transaction = {};
-                            transaction.date1 = '';
-                            transaction.creditAccount = 'By Balance C/D';
-                            transaction.creditValue = node.balance;
-                            transaction.date2 = '';
-                            transaction.debitAccount = '';
-                            transaction.debitValue = '';
-                            node.transactions.push(transaction);
-                        }
-                    }
-                    else {
-                        for (var i = 0; i < node.dr.length; i++) {
-                            var transaction = {};
-                            transaction.date1 = new Date(node.dr[i].date.date);
-                            transaction.creditAccount = "By " + node.dr[i].account;
-                            transaction.creditValue = node.dr[i].value;
-                            transaction.date2 = new Date(node.cr[i].date.date);
-                            transaction.debitAccount = "To " + node.cr[i].account;
-                            transaction.debitValue = node.cr[i].value;
-                            node.transactions.push(transaction);
-                        }
-
-                        if (node.cr_balance < node.dr_balance) {
-                            var transaction = {};
-                            transaction.date1 = '';
-                            transaction.creditAccount = 'By Balance C/D';
-                            transaction.creditValue = node.balance;
-                            transaction.date2 = '';
-                            transaction.debitAccount = '';
-                            transaction.debitValue = '';
-                            node.transactions.push(transaction);
-                        }
-                        else if (node.cr_balance > node.dr_balance) {
-                            var transaction = {};
-                            transaction.date1 = '';
-                            transaction.creditAccount = '';
-                            transaction.creditValue = '';
-                            transaction.date2 = '';
-                            transaction.debitAccount = 'To Balance C/D';
-                            transaction.debitValue = node.balance;
-                            node.transactions.push(transaction);
-                        }
-                    }
+                $scope.ledger.dr = Object.keys($scope.ledger.dr).map(function (k) { return $scope.ledger.dr[k] });
+                $scope.ledger.cr = Object.keys($scope.ledger.cr).map(function (k) { return $scope.ledger.cr[k] });
+                $scope.ledger.credit = 0;
+                $scope.ledger.debit = 0;
+                $scope.ledger.dr.forEach(function(node){
+                    node.date = new Date(node.date.date);
+                    $scope.ledger.debit+=node.value;
                 });
-
+                $scope.ledger.cr.forEach(function(node){
+                    node.date = new Date(node.date.date);
+                    $scope.ledger.credit+=node.value;
+                });
                 $scope.showLedgers = true;
-                $scope.debit = 0;
-                $scope.credit = 0;
-                $scope.ledgers.forEach(function (node) {
-                    if (node.cr_balance > node.dr_balance) {
-                        $scope.credit += node.balance;
-                    }
-                    else {
-                        $scope.debit += node.balance;
-                    }
-                });
+                console.log($scope.ledger);
 
-                //console.log($scope.ledgers);
-            });
+            //     $scope.ledgers.forEach(function (node) {
+            //         node.transactions = [];
+            //         if (node.opening_balance_type=="Dr") {
+            //             var transaction = {};
+            //             transaction.date1 = '';
+            //             transaction.creditAccount = 'By Balance C/D';
+            //             transaction.creditValue = node.balance;
+            //             transaction.date2 = '';
+            //             transaction.debitAccount = '';
+            //             transaction.debitValue = '';
+            //             node.transactions.push(transaction);
+            //         }
+            //         else if (node.cr_balance > node.dr_balance) {
+            //             var transaction = {};
+            //             transaction.date1 = '';
+            //             transaction.creditAccount = '';
+            //             transaction.creditValue = '';
+            //             transaction.date2 = '';
+            //             transaction.debitAccount = 'To Balance C/D';
+            //             transaction.debitValue = node.balance;
+            //             node.transactions.push(transaction);
+            //         }
+            //         node.dr = Object.keys(node.dr).map(function (k) { return node.dr[k] });
+            //         node.cr = Object.keys(node.cr).map(function (k) { return node.cr[k] });
+            //         if (node.cr.length > node.dr.length) {
+            //             for (var i = 0; i < node.cr.length; i++) {
+            //                 var transaction = {};
+            //                 transaction.date1 = new Date(node.cr[i].date.date);
+            //                 transaction.debitAccount = "To " + node.cr[i].account;
+            //                 transaction.debitValue = node.cr[i].value;
+            //                 if (node.dr.length > i) {
+            //                     transaction.date2 = new Date(node.dr[i].date.date);
+            //                     transaction.creditAccount = "By " + node.dr[i].account;
+            //                     transaction.creditValue = node.dr[i].value;
+            //                 }
+            //                 else if (node.dr.length == i && node.dr_balance > node.cr_balance) {
+            //                     transaction.date2 = '';
+            //                     transaction.creditAccount = 'By Balance C/D';
+            //                     transaction.creditValue = node.balance;
+            //                 }
+            //                 else {
+            //                     transaction.date2 = '';
+            //                     transaction.creditAccount = '';
+            //                     transaction.creditValue = '';
+            //                 }
+            //                 node.transactions.push(transaction);
+            //             }
+            //             if (node.cr_balance > node.dr_balance) {
+            //                 var transaction = {};
+            //                 transaction.date1 = '';
+            //                 transaction.creditAccount = '';
+            //                 transaction.creditValue = '';
+            //                 transaction.date2 = '';
+            //                 transaction.debitAccount = 'To Balance C/D';
+            //                 transaction.debitValue = node.balance;
+            //                 node.transactions.push(transaction);
+            //             }
+            //         }
+            //         else if (node.cr.length < node.dr.length) {
+            //             for (var i = 0; i < node.dr.length; i++) {
+            //                 var transaction = {};
+            //                 transaction.date2 = new Date(node.dr[i].date.date);
+            //                 transaction.creditAccount = "By " + node.dr[i].account;
+            //                 transaction.creditValue = node.dr[i].value;
+            //                 if (node.cr.length > i) {
+            //                     transaction.date1 = new Date(node.cr[i].date.date);
+            //                     transaction.debitAccount = "To " + node.cr[i].account;
+            //                     transaction.debitValue = node.cr[i].value;
+            //                 }
+            //                 else if (node.cr.length == i && node.cr_balance > node.dr_balance) {
+            //                     transaction.date1 = '';
+            //                     transaction.debitAccount = 'To Balance C/D';
+            //                     transaction.debitValue = node.balance;
+            //                 }
+            //                 else {
+            //                     transaction.date1 = '';
+            //                     transaction.debitAccount = '';
+            //                     transaction.debitValue = '';
+            //                 }
+            //                 node.transactions.push(transaction);
+            //             }
+            //             if (node.cr_balance < node.dr_balance) {
+            //                 var transaction = {};
+            //                 transaction.date1 = '';
+            //                 transaction.creditAccount = 'By Balance C/D';
+            //                 transaction.creditValue = node.balance;
+            //                 transaction.date2 = '';
+            //                 transaction.debitAccount = '';
+            //                 transaction.debitValue = '';
+            //                 node.transactions.push(transaction);
+            //             }
+            //         }
+            //         else {
+            //             for (var i = 0; i < node.dr.length; i++) {
+            //                 var transaction = {};
+            //                 transaction.date1 = new Date(node.dr[i].date.date);
+            //                 transaction.creditAccount = "By " + node.dr[i].account;
+            //                 transaction.creditValue = node.dr[i].value;
+            //                 transaction.date2 = new Date(node.cr[i].date.date);
+            //                 transaction.debitAccount = "To " + node.cr[i].account;
+            //                 transaction.debitValue = node.cr[i].value;
+            //                 node.transactions.push(transaction);
+            //             }
+
+            //             if (node.cr_balance < node.dr_balance) {
+            //                 var transaction = {};
+            //                 transaction.date1 = '';
+            //                 transaction.creditAccount = 'By Balance C/D';
+            //                 transaction.creditValue = node.balance;
+            //                 transaction.date2 = '';
+            //                 transaction.debitAccount = '';
+            //                 transaction.debitValue = '';
+            //                 node.transactions.push(transaction);
+            //             }
+            //             else if (node.cr_balance > node.dr_balance) {
+            //                 var transaction = {};
+            //                 transaction.date1 = '';
+            //                 transaction.creditAccount = '';
+            //                 transaction.creditValue = '';
+            //                 transaction.date2 = '';
+            //                 transaction.debitAccount = 'To Balance C/D';
+            //                 transaction.debitValue = node.balance;
+            //                 node.transactions.push(transaction);
+            //             }
+            //         }
+            //     });
+
+            //     
+            //     $scope.debit = 0;
+            //     $scope.credit = 0;
+            //     $scope.ledgers.forEach(function (node) {
+            //         if (node.cr_balance > node.dr_balance) {
+            //             $scope.credit += node.balance;
+            //         }
+            //         else {
+            //             $scope.debit += node.balance;
+            //         }
+            //     });
+
+            //     //console.log($scope.ledgers);
+             });
     }
 
     // $scope.showTrialBalance = function (){
